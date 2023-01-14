e170ed13e43f2ea3277477c1ae991c3e06e7ee75
Quang Nguyen <quang13012005@gmail.com>
2023-01-14 08:44:46 +0700

    Update main.rs

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,7 @@
 fn main() {
     println!("Hello, world!");
-}
+} 
+//SUMMARY CHAPTER 1
+//to create main file (binary file - unreaddable) -> type $rustc main.rs 
+//to compile and run and execute code -> ./main 
+//REMEMBER TO CHANGE THE DIRECTORY EACH TIME YOU SWITCH FOLDERS (type $cd FOLDER_NAME or $cd .. to back out of the folder)
