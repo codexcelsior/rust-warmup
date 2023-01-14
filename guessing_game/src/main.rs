6facb70c2af96b12df1d6ab7a6aeeadab98dc064
Quang Nguyen <quang13012005@gmail.com>
2023-01-14 12:18:30 +0700

    Finished Chapter 2: Guessing Game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,32 +4,36 @@ use std::cmp::Ordering; //Using Standard compare ordering
 
 fn main() {
 
-    println!("Guess the number!"); //print line Guess the number
+    
+    loop {
+        println!("Guess the number!"); //print line Guess the number
+    
+        let secret_number = rand::thread_rng().gen_range(1..=100); //Generate a secret number by randoming a number
+    
+        // println!("The secret number is {secret_number}"); //Cheat line here hehe
+        println!("Please input your guess."); //print line Please input your guess
+    
+        let mut guess = String::new(); //create a new string named guess 
+    
+        io::stdin() 
+            .read_line(&mut guess) //using read_line to pass a guess into a string
+            .expect("Failed to read line"); //expect to print out Failed to read line if the number is error
+            
+        //  --snip--
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");// trim() use to trim space character, parse() use to convert a string into a number 
+        
+        println!("You guessed: {guess}"); 
+        
 
-    let secret_number = rand::thread_rng().gen_range(1..=100); //Generate a secret number by randoming a number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!"); //Announce winner 
+                break; //Quitting if win
+        }
+        }
 
-    println!("Please input your guess."); //print line Please input your guess
-
-    let mut guess = String::new(); //create a new string named guess 
-
-    io::stdin() 
-        .read_line(&mut guess) //using read_line to pass a guess into a string
-        .expect("Failed to read line"); //expect to print out Failed to read line if the number is error
-
-    //  --snip--
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {guess}"); 
-
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
     }
 
-
-    // let x = 5;
-    // let y = 10;
-    // println!("x is {x} and y + 2 = {}", y + 2);    
 }
